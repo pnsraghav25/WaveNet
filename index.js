15198d49149dcbfa3639620a0c2f4d9077863e91
Peddireddy Naga Sai Raghav <pnsraghav25@gmail.com>
2023-08-13 07:19:46 +0530

    Deployment2

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ main.addEventListener("mousemove",function(dets){
     crsr.style.left =dets.x+"px"
     crsr.style.top =dets.y+"px"
 })
+
+const menu_icon = document.querySelector(".menu_icon")
+menu_icon.addEventListener("click",()=>{
+    const menu = menu_icon.querySelector(".menu_items")
+    menu.style.display = "flex"
+    console.log("flex")
+})
