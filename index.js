65ab5af08af95332981ec043e0c0d77f73e241fb
Peddireddy Naga Sai Raghav <pnsraghav25@gmail.com>
2023-08-13 06:40:56 +0530

    Deployment

---
+++ index.js
@@ -0,0 +1,6 @@
+var crsr = document.querySelector(".cursor")
+var main = document.querySelector(".main")
+main.addEventListener("mousemove",function(dets){
+    crsr.style.left =dets.x+"px"
+    crsr.style.top =dets.y+"px"
+})
